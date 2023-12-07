292d930ef782fa1a4686cf52642b897a6637b3ee
Renato_Rongone <r.rongone17gmail.com>
2023-12-07 16:50:18 +0100

    soluzione nav non trovata

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-sm navbar-dark bgNavbar">
-    <div class="container-fluid ms-md-5">
+<nav class="navbar navbarCustom navbar-expand-sm navbar-dark bgNavbar">
+    <div class="container-fluid ms-md-0">
         <a class="navbar-brand textMyPurple" href="#">logo</a>
         <form role="search">
             <input class="form-control" type="search" placeholder="Search" aria-label="Search">
@@ -8,8 +8,8 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="prestoNavbar">
-            <ul class="navbar-nav ms-auto mb-2 mb-sm-0 me-md-5">
+        <div class="collapse bg-success navbar-collapse" id="prestoNavbar">
+            <ul class="navbar-nav w-75 dropdownCustom mb-2 mb-sm-0 me-md-0">
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="#">Home</a>
                 </li>
@@ -17,11 +17,20 @@
                     <a class="nav-link" href="#">Link</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                    <ul class="dropdown-menu">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Admin</a>
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
                         <li><a class="dropdown-item" href="#">Action</a></li>
                         <li><a class="dropdown-item" href="#">Another action</a></li>
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">User</a>
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start">
+                        <li><a class="dropdown-item" href="#">Profilo</a></li>
+                        <li><a class="dropdown-item" href="#">Registrati</a></li>
+                        <li><a class="dropdown-item" href="#">Accedi</a></li>
+                        <li><a class="dropdown-item" href="#">Esci</a></li>
                     </ul>
                 </li>
             </ul>
