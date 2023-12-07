73357213eecda1db547f87d527c8c47849e3f8ee
Luca Addario <iamlucaaddario@gmail.com>
2023-12-07 16:58:28 +0100

    Bug Fixed Navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,15 @@
 <nav class="navbar navbar-expand-sm navbar-dark bgNavbar">
     <div class="container-fluid ms-md-5">
         <a class="navbar-brand textMyPurple" href="#">logo</a>
-        <form role="search">
+        <form role="search" class="formNavbar">
             <input class="form-control" type="search" placeholder="Search" aria-label="Search">
         </form>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#prestoNavbar" aria-controls="prestoNavbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="prestoNavbar">
-            <ul class="navbar-nav ms-auto mb-2 mb-sm-0 me-md-5">
+        <div class="collapse position-relative  navbar-collapse" id="prestoNavbar">
+            <ul class="navbar-nav ms-auto mb-2 mb-sm-0 me-md-5 linkContainer">
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="#">Home</a>
                 </li>
