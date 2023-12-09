819907ed37ce2a152c288cc08c8f126fc17a0e09
jacopovalli <jacopo.valli.personale@gmail.com>
2023-12-09 21:45:16 +0100

    inizializzato front-end blade login e register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,14 @@
 <x-layout>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 py-5 bg-warning">
                 <h1 class="text-center">Accedi</h1>
             </div>
         </div>
     </div>
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-6">
+            <div class="col-6 py-5 bg-info">
                 @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -21,18 +21,20 @@
                 <form method="POST" action="/login">
                     @csrf
                     <div class="mb-3">
-                        <label for="email" class="form-label">Indirizzo email</label>
+                        <label for="email" class="form-label">email</label>
                         <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
                     </div>
                     <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
+                        <label for="password" class="form-label">password</label>
                         <input name="password" type="password" class="form-control" id="password">
                     </div>
                     <div class="mb-3 form-check">
                         <input name="remember" type="checkbox" class="form-check-input" id="remember">
-                        <label class="form-check-label" for="remember">Ricordami</label>
+                        <label class="form-check-label" for="remember">ricordami</label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <div class="d-flex justify-content-center py-4">
+                        <button type="submit" class="btn bgMyPurple">accedi</button>
+                    </div>
                 </form>
             </div>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,54 @@
 <x-layout>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 py-5 bg-warning">
                 <h1 class="text-center">Registrati</h1>
             </div>
         </div>
     </div>
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-6">
+            <div class="col-6 py-5 bg-info">
                 <form method="POST" action="/register">
                     @csrf
                     <div class="mb-3">
-                        <label for="name" class="form-label">Nome</label>
+                        <label for="name" class="form-label">nome</label>
                         <input name="name" type="text" class="form-control" id="name">
                     </div>
                     <div class="mb-3">
-                        <label for="surname" class="form-label">Cognome</label>
+                        <label for="surname" class="form-label">cognome</label>
                         <input name="surname" type="text" class="form-control" id="surname">
                     </div>
                     <div class="mb-3">
-                        <label for="birth" class="form-label">Data di Nascita</label>
+                        <label for="birth" class="form-label">data di nascita</label>
                         <input name="birth" type="date" class="form-control" id="birth">
                     </div>
                     <div class="mb-3">
-                        <label class="d-block" for="gender">Genere</label>
+                        <label class="d-block" for="gender">genere</label>
                         <select name="gender" id="gender">
-                            <option value="M">Uomo</option>
-                            <option value="F">Donna</option>
+                            <option value="M">uomo</option>
+                            <option value="F">donna</option>
                         </select>
                     </div>
                     <div class="mb-3">
-                        <label for="telephone" class="form-label">Numero di Telefono</label>
+                        <label for="telephone" class="form-label">telefono</label>
                         <input name="telephone" type="text" class="form-control" id="telephone">
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Indirizzo email</label>
+                        <label for="email" class="form-label">email</label>
                         <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
                     </div>
                     <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
+                        <label for="password" class="form-label">password</label>
                         <input name="password" type="password" class="form-control" id="password">
                     </div>
                     <div class="mb-3">
-                        <label for="password_confirmation" class="form-label">Conferma Password</label>
+                        <label for="password_confirmation" class="form-label">conferma password</label>
                         <input name="password_confirmation" type="password" class="form-control" id="password_confirmation">
                     </div>
-                    <button type="submit" class="btn btn-primary">Registrati</button>
+                    <div class="d-flex justify-content-center py-4">
+                        <button type="submit" class="btn bgMyPurple">registrati</button>
+                    </div>
                 </form>
             </div>
         </div>
