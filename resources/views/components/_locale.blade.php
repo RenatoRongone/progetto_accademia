ff75930ba70133783126b248696719dc50368205
jacopovalli <jacopo.valli.personale@gmail.com>
2023-12-18 18:19:02 +0100

    sistemazione messaggi di ritorno in vista in lingua; dettagli front-end

--- resources/views/components/_locale.blade.php
+++ resources/views/components/_locale.blade.php
@@ -1,6 +1,6 @@
 <form method="POST" action="{{route('setLocale', $lang)}}">
     @csrf
-    <button type="submit" >
+    <button type="submit" class="btn">
         <img src="{{asset('vendor/blade-flags/language-' . $lang . '.svg')}}" alt="" width="28" height="28">
     </button>
 </form>
