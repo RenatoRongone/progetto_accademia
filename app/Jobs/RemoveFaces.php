9cb0f85491cae86209d6a8cfcd8e3a9feba85bdc
jacopovalli <jacopo.valli.personale@gmail.com>
2023-12-19 20:17:47 +0100

    User Story 8 modificata e ultimata

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -16,9 +16,9 @@ use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 class RemoveFaces implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
+    
     private $announcement_image_id;
-
+    
     /**
     * Create a new job instance.
     */
@@ -26,7 +26,7 @@ class RemoveFaces implements ShouldQueue
     {
         $this->announcement_image_id = $announcement_image_id;
     }
-
+    
     /**
     * Execute the job.
     */
@@ -38,35 +38,36 @@ class RemoveFaces implements ShouldQueue
         }
         $srcPath = storage_path('app/public/' . $i->path);
         $image = file_get_contents($srcPath);
-
+        
         putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
-
+        
         $imageAnnotator = new ImageAnnotatorClient();
         $response = $imageAnnotator->faceDetection($image);
         $faces = $response->getFaceAnnotations();
-
-
-
+        
+        
+        
         foreach ($faces as $face){
             $vertices = $face->getBoundingPoly()->getVertices();
-
+            
             $bounds = [];
             foreach($vertices as $vertex){
-            $bounds[] = [$vertex->getX(), $vertex->getY()];
+                $bounds[] = [$vertex->getX(), $vertex->getY()];
+            }
+            
             $w = $bounds[2][0] - $bounds [0][0];
             $h = $bounds[2][1] - $bounds [0][1];
-
+            
             $image = SpatieImage::load($srcPath);
-
+            
             $image->watermark(base_path('resources/img/smile.png'))
             ->watermarkPosition('top-left')
             ->watermarkPadding($bounds[0][0] , $bounds[0][1])
             ->watermarkWidth($w, Manipulations::UNIT_PIXELS)
             ->watermarkHeight($h, Manipulations::UNIT_PIXELS)
             ->watermarkFit(Manipulations::FIT_STRETCH);
-
+            
             $image->save($srcPath);
-            }
         }
         $imageAnnotator->close();
     }
